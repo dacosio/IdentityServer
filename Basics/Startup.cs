92d6df799174aa9f8cc1d2653d52e569f3d6d1fd
Don Apollo Cosio <donapollocosio@Demsens-iMac.local>
2020-05-25 16:37:58 +0800

    auth

---
+++ Basics/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+
+namespace Basics
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+                });
+
+          
+
+            services.AddControllersWithViews();
+
+          
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+          
+            // who are you?
+            app.UseAuthentication();
+
+            // are you allowed?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+             
+            });
+        }
+    }
+}
